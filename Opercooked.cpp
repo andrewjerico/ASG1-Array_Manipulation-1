42345c90d453127f76b646e4574fd50c644809f1
Lezon <leonard.zonaphan@binus.ac.id>
2020-10-29 22:48:23 +0700

    test

---
+++ Opercooked.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+
+#include "object.h"
+#include "other.h"
+#include "string.h"
+
+int orderNum=0;
+
+int main() {
+    getTime();
+    return 0;
+}
---
+++ object.h
@@ -0,0 +1,9 @@
+struct Dessert {
+    char name[255];
+    int price;
+    char topping[25];
+    double calories;
+    char flavour[25];
+    char size;
+    int time;
+};
