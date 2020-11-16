c06394a8487bc1b40d7d91557eaeb5fad68cdea6
blackjackals <65567021+blackjackals@users.noreply.github.com>
2020-11-16 21:50:17 +0700

    Update Opercooked.cpp

--- Opercooked.cpp
+++ Opercooked.cpp
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 #include "object.h"
 #include "other.h"
 #include "string.h"
 
-int orderNum=0;
+#include "menu1.h"
+#include "menu2.h"
+#include "menu3.h"
+#include "menu4.h"
+#include "mainMenu.h"
 
-int main() {
-    getTime();
+
+int main() 
+{
+    mainMenu();
     return 0;
 }
