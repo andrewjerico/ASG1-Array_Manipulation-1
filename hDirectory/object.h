3cd70a9063a5f020baf55ab6c4e1aeee7d3dd9e6
Lezonnn <73393716+Lezonnn@users.noreply.github.com>
2020-11-18 19:07:50 +0700

    Update object.h
    add global variable

--- hDirectory/object.h
+++ hDirectory/object.h
@@ -1,13 +1,25 @@
-struct Order {
+struct Order
+{
     char name[255];
     int price;
-    char topping[20];
+    char topping[25];
     double calories;
-    char flavor[20];
+    char flavor[25];
     char size;
     int time;
     char orderTime[128];
+    char type[20];
 };
 
-struct Order order[255];
-struct Order history[255];
+Order order[255];
+Order history[255];
+
+char topping[3][20] = {"Caramel", "Honey", "Syrup"};
+char flavor[3][20] = {"Mint", "Mix Berry", "Cheese"};
+
+int orderNum = 0;
+int historyNum = 0;
+int cookingNum = 0;
+int profit = 0;
+int orderNo = 0;
+int cookDone[255] = {};
